13867acb56845f681ee466c7eb58767e00ba7e9e
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-11 00:17:37 -0300

    Criando todas as pastas e estrutura de arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Header from "./components/Header";
+
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Deus é mais</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer  />  */}
+
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
