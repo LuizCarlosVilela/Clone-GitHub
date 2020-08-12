9c7dffd87aa49866d25b51732ab45226ee9f7f62
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-11 21:07:44 -0300

    Criando estrutura para listar os repositórios do usuário

--- src/components/RepoCard/index.tsx
+++ src/components/RepoCard/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Topside,
+  RepoIcon,
+  BootSide,
+  StartIcon,
+  ForkIcon,
+} from "./styles";
+
+import { Link } from "react-router-dom";
 
 interface Props {
   username: string;
@@ -19,7 +28,39 @@ const RepoCard: React.FC<Props> = ({
   starts,
   forks,
 }) => {
-  return <Container></Container>;
+  const languageClass = language ? language.toLowerCase() : "other";
+
+  return (
+    <Container>
+      <Topside>
+        <header>
+          <RepoIcon />
+          <Link to={`/${username}/${reponame}`}>{reponame}</Link>
+        </header>
+
+        <p>{description}</p>
+      </Topside>
+
+      <BootSide>
+        <ul>
+          <li>
+            <div className={`language ${languageClass}`} />
+            <span>{language}</span>
+          </li>
+
+          <li>
+            <StartIcon />
+            <span>{starts}</span>
+          </li>
+
+          <li>
+            <ForkIcon />
+            <span>{forks}</span>
+          </li>
+        </ul>
+      </BootSide>
+    </Container>
+  );
 };
 
 export default RepoCard;
--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Topside = styled.div``;
+
+export const RepoIcon = styled.div``;
+
+export const BootSide = styled.div``;
+
+export const StartIcon = styled.div``;
+
+export const ForkIcon = styled.div``;
