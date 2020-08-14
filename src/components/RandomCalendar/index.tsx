2d6be775aa974a85af53da661d4a8a60270de81a
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-13 23:47:52 -0300

    Alterando marcações no calendário

--- src/components/RandomCalendar/index.tsx
+++ src/components/RandomCalendar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Heatmap from "react-calendar-heatmap";
 
-import { subYears, isBefore, isSameDay, addDays } from "date-fns";
+import { subYears, isBefore, isSameDay, addDays, subDays } from "date-fns";
 
 import { Container } from "./styles";
 
@@ -14,6 +14,57 @@ const RandomCalendar: React.FC = () => {
   const values: HeatmapValue[] = [];
 
   values.push({ date: new Date(), count: 3 });
+  values.push({ date: subDays(new Date(), 2), count: 3 });
+  values.push({ date: subDays(new Date(), 311), count: 3 });
+  values.push({ date: subDays(new Date(), 310), count: 3 });
+  values.push({ date: subDays(new Date(), 309), count: 3 });
+  values.push({ date: subDays(new Date(), 308), count: 3 });
+  values.push({ date: subDays(new Date(), 307), count: 3 });
+  values.push({ date: subDays(new Date(), 306), count: 3 });
+  values.push({ date: subDays(new Date(), 305), count: 3 });
+
+  values.push({ date: subDays(new Date(), 298), count: 3 });
+  values.push({ date: subDays(new Date(), 291), count: 3 });
+
+  values.push({ date: subDays(new Date(), 269), count: 3 });
+  values.push({ date: subDays(new Date(), 268), count: 3 });
+  values.push({ date: subDays(new Date(), 267), count: 3 });
+  values.push({ date: subDays(new Date(), 266), count: 3 });
+  values.push({ date: subDays(new Date(), 265), count: 3 });
+  values.push({ date: subDays(new Date(), 264), count: 3 });
+
+  values.push({ date: subDays(new Date(), 270), count: 3 });
+  values.push({ date: subDays(new Date(), 263), count: 3 });
+
+  values.push({ date: subDays(new Date(), 277), count: 3 });
+  values.push({ date: subDays(new Date(), 278), count: 3 });
+  values.push({ date: subDays(new Date(), 279), count: 3 });
+  values.push({ date: subDays(new Date(), 280), count: 3 });
+  values.push({ date: subDays(new Date(), 281), count: 3 });
+  values.push({ date: subDays(new Date(), 282), count: 3 });
+  values.push({ date: subDays(new Date(), 283), count: 3 });
+
+  values.push({ date: subDays(new Date(), 249), count: 3 });
+  values.push({ date: subDays(new Date(), 250), count: 3 });
+  values.push({ date: subDays(new Date(), 251), count: 3 });
+  values.push({ date: subDays(new Date(), 252), count: 3 });
+  values.push({ date: subDays(new Date(), 253), count: 3 });
+  values.push({ date: subDays(new Date(), 254), count: 3 });
+  values.push({ date: subDays(new Date(), 255), count: 3 });
+
+  values.push({ date: subDays(new Date(), 241), count: 3 });
+  values.push({ date: subDays(new Date(), 234), count: 3 });
+  values.push({ date: subDays(new Date(), 227), count: 3 });
+
+  values.push({ date: subDays(new Date(), 226), count: 3 });
+  values.push({ date: subDays(new Date(), 225), count: 3 });
+
+  values.push({ date: subDays(new Date(), 237), count: 3 });
+  values.push({ date: subDays(new Date(), 236), count: 3 });
+  values.push({ date: subDays(new Date(), 235), count: 3 });
+
+  values.push({ date: subDays(new Date(), 215), count: 3 });
+  values.push({ date: subDays(new Date(), 205), count: 3 });
 
   return (
     <Container>
