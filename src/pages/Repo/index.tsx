a5c0eec723a585c268e76c95add32bbf25e8d1bd
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-16 21:13:09 -0300

    Fazendo a tela de repositorios, arquitetura e style

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,12 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GitHubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
   return (
-    <>
-      <h1>Repo</h1>
-    </>
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={"username"} to={"/guilhermerodz"}>
+          guilermerodz
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/guilhermerodz/youtube-content"}>
+          youtube-content
+        </Link>
+      </Breadcrumb>
+
+      <p>Contains all of my Youtube lessons code.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>starts</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/LuizCarlosVilela/clone-github"}>
+        <GitHubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
   );
 };
 
