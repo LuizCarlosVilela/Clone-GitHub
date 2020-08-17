1f7bcc628a6bf595b0ef42cb229ec3e4f985933f
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-16 23:20:36 -0300

    Fazendo search por perfil funcionar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Container, GithubLogo, SearchForm } from "./styles";
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function hadleSubmit(event: React.FormEvent) {
+    // event.preventDefault();
+    navigate("/" + search.toLocaleLowerCase().trim());
+  }
+
   return (
     <Container>
       <GithubLogo />
 
-      <SearchForm>
-        <input placeholder="Enter Username or Repoo" type="text" />
+      <SearchForm onSubmit={hadleSubmit}>
+        <input
+          placeholder="Enter Username or Repoo"
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
